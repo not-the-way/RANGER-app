6fea8043557364e8ca3310c4f44f0d5aa26e6b7d
not-the-way <txajbraxmanov@mail.ru>
2025-10-16 01:03:45 +0300

    Useless addition 2

---
+++ CORE/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// Наверное, мне это не понадобится
+
+namespace RANGER.CORE
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName  = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
