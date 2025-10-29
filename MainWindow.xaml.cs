393b7103651c12741a30fc95d20a3f921de66ab3
not-the-way <txajbraxmanov@mail.ru>
2025-10-29 23:24:59 +0300

    Started doing some logic for the main window (there is sooo much more work to do on showing the data)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RANGER.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static RANGER.Database.DatabaseImplementation;
 
 namespace RANGER
 {
@@ -23,9 +25,45 @@ namespace RANGER
         public MainWindow()
         {
             InitializeComponent();
-            LoginWindow login = new LoginWindow();
+            UpdateWhoIsLoggedStatus();
+        }
 
-            login.Show();
+
+        // Метод должен изменять текст в LoggedUserTextBox на имя пользователя
+        public void UpdateWhoIsLoggedStatus()
+        {
+            var whoIsLogged = LoggedUserTextBox;
+            var dbNameReader = new DatabaseImplementation().ExecuteQuery<Employee>(@"SELECT * FROM Employee;");
+
+            // string userFullName = dbNameReader.Where(n => n.FullName.Equals("");
+            // whoIsLogged.Text = $"Пользователь: {userFullName}";
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Код ниже отвечает за выход из приложения
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // При закрытии окна показывается MessageBox с кнопками да и нет
+            var mBox = MessageBox.Show("Вы точно хотите выйти?",
+                                       "Выход из приложения",
+                                       MessageBoxButton.YesNo,
+                                       MessageBoxImage.Question,
+                                       MessageBoxResult.No);
+
+            // Здесь нужно реализовать выход приложения при нажатии "Да"
+            // И продолжение работы приложения при "Нет"
+            if (mBox == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
         }
     }
 }
