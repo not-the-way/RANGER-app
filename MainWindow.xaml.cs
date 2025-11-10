eeb3789017aff4f4babaf8cfc03fe47e1e6e6af5
not-the-way <txajbraxmanov@mail.ru>
2025-11-10 21:40:49 +0300

    Added data loaders for ListView

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,13 +28,13 @@ namespace RANGER
     {
         private DataBaseConnection _database;
         private string currentTable;
+        private string currentAccessLevel;
 
         public MainWindow(Employee employee)
         {
             InitializeComponent();
 
             LoggedUserTextBox.Text = $"Пользователь: {employee.FullName}";
-            AccessLevelTextBox.Text = $"Вход как: {employee.AccessLevel}";
 
             currentTable = "Employee";
 
@@ -50,7 +50,8 @@ namespace RANGER
             switch (employee.AccessLevel)
             {
                 case "Admin":
-                    
+                    currentAccessLevel = "Сис. Админ";
+
                     ClientsRBtn.Visibility = Visibility.Visible;
                     FirearmsRBtn.Visibility = Visibility.Visible;
                     IssueRBtn.Visibility = Visibility.Visible;
@@ -60,7 +61,8 @@ namespace RANGER
                     
 
                 case "Manager":
-                    
+                    currentAccessLevel = "Менеджер тира";
+
                     ClientsRBtn.Visibility = Visibility.Visible;
                     FirearmsRBtn.Visibility = Visibility.Visible;
                     IssueRBtn.Visibility = Visibility.Visible;
@@ -69,15 +71,21 @@ namespace RANGER
                     break;
 
                 case "Warehouse":
+                    currentAccessLevel = "Сотрудник склада";
+
                     FirearmsRBtn.Visibility = Visibility.Visible;
                     WarehouseRBtn.Visibility = Visibility.Visible;
                     break;
 
                 case "Issue":
+                    currentAccessLevel = "Сотрудник выдачи";
+
                     IssueRBtn.Visibility = Visibility.Visible;
                     ClientsRBtn.Visibility = Visibility.Visible;
                     break;
             }
+
+            AccessLevelTextBox.Text = $"Вход как: {currentAccessLevel}";
         }
 
         // Код ниже отвечает за выход из приложения
@@ -118,12 +126,25 @@ namespace RANGER
         }
         private void ClientsRBtn_Checked(object sender, RoutedEventArgs e)
         {
-
+            currentTable = "Client";
+            LoadData();
         }
 
         private void FirearmsRBtn_Checked(object sender, RoutedEventArgs e)
         {
             currentTable = "Firearm";
+            LoadData();
+        }
+
+        private void IssueRBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            currentTable = "Issue";
+            LoadData();
+        }
+
+        private void WarehouseRBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            currentTable = "Warehouse";
             LoadData();
         }
 
@@ -141,18 +162,18 @@ namespace RANGER
                     case "Employee":
                         LoadDataForEmployees();
                         break;
-                    //case "Client":
-                    //    LoadDataForClients();
-                    //    break;
+                    case "Client":
+                        LoadDataForClients();
+                        break;
                     case "Firearm":
                         LoadDataForFirearms();
                         break;
                     case "Issue":
                         LoadDataForIssues();
                         break;
-                    //case "Warehouse":
-                    //    LoadDataForWarehouse();
-                    //    break;
+                    case "Warehouse":
+                        LoadDataForWarehouse();
+                        break;
                 }
             }
             catch (Exception ex)
@@ -256,16 +277,65 @@ namespace RANGER
             }
         }
 
+        private void LoadDataForClients()
+        {
+            try
+            {
+                var clients = _database.ExecuteQuery<Client>("SELECT * FROM Client");
+                var gridView = (GridView)dataListView.View;
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Номер клиента",
+                    DisplayMemberBinding = new Binding("Client_ID")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "ФИО клиента",
+                    DisplayMemberBinding = new Binding("FullName")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Серия и номер паспорта",
+                    DisplayMemberBinding = new Binding("Passport")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Совершеннолетний",
+                    DisplayMemberBinding = new Binding("IsMature")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Подписан отказ от ответственности",
+                    DisplayMemberBinding = new Binding("ReleaseOfLiability")
+                });
+
+                dataListView.ItemsSource = clients;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки данных: {ex.Message}", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
         private void LoadDataForIssues()
         {
             try
             {
-                var issues = _database.ExecuteQuery<Issue>(@"SELECT I.Issue_ID, E.FullName, C.FullName, F.Name, W.ItemName, I.DateTimeOfIssue, I.DateTimeOfReturn
-                                                           FROM (((Issue I INNER JOIN Employee E ON I.Employee_ID = E.Employee_ID)
-                                                           INNER JOIN Client C ON I.Client_ID = C.Client_ID)
-                                                           INNER JOIN Firearm F ON I.Firearm_SerialNumber = F.FirearmSerialNumber)
-                                                           INNER JOIN Warehouse W ON I.Item_ID = W.Item_ID");
-
+                var issues = _database.ExecuteQuery<Issue>
+                ("SELECT i.Issue_ID, e.FullName as Employee_FullName, c.Fullname as Client_Fullname, " +
+                "f.Name as Firearm_Name, w.ItemName, i.DateTimeOfIssue, i.DateTimeOfReturn " +
+                "FROM Issue i " +
+                "LEFT JOIN Client c ON i.Client_ID = c.Client_ID " +
+                "LEFT JOIN Employee e ON i.Employee_ID = e.Employee_ID " +
+                "LEFT JOIN Firearm f ON i.Firearm_SerialNumber = f.FirearmSerialNumber " +
+                "LEFT JOIN Warehouse w ON i.Item_ID = w.Item_ID ");
+                
                 var gridView = (GridView)dataListView.View;
 
                 gridView.Columns.Add(new GridViewColumn
@@ -277,38 +347,40 @@ namespace RANGER
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "ФИО сотрудника",
-                    DisplayMemberBinding = new Binding("FullName")
+                    DisplayMemberBinding = new Binding("Employee.FullName")
                 });
 
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "ФИО клиента",
-                    DisplayMemberBinding = new Binding("C.FullName")
+                    DisplayMemberBinding = new Binding("Client.FullName")
                 });
 
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "Название оружия",
-                    DisplayMemberBinding = new Binding("F.Name")
+                    DisplayMemberBinding = new Binding("Firearm.Name")
                 });
 
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "Название предмета",
-                    DisplayMemberBinding = new Binding("W.ItemName")
+                    DisplayMemberBinding = new Binding("Warehouse.ItemName")
                 });
 
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "Дата выдачи",
-                    DisplayMemberBinding = new Binding("I.DateTimeOfIssue")
+                    DisplayMemberBinding = new Binding("DateTimeOfIssue")
                 });
 
                 gridView.Columns.Add(new GridViewColumn
                 {
                     Header = "Дата возврата",
-                    DisplayMemberBinding = new Binding("I.DateTimeOfReturn")
+                    DisplayMemberBinding = new Binding("DateTimeOfReturn")
                 });
+
+                dataListView.ItemsSource = issues;
             }
             catch (Exception ex)
             {
@@ -317,5 +389,55 @@ namespace RANGER
             }
         }
 
+        private void LoadDataForWarehouse()
+        {
+            try
+            {
+                var warehouse = _database.ExecuteQuery<Warehouse>
+                ("SELECT w.*, wt.Type as WarehouseType " +
+                "FROM Warehouse w " +
+                "LEFT JOIN WarehouseType wt ON w.WarehouseType = wt.Type " +
+                "ORDER BY w.Item_ID");
+
+                var gridView = (GridView)dataListView.View;
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Номер предмета",
+                    DisplayMemberBinding = new Binding("Item_ID")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Наименование",
+                    DisplayMemberBinding = new Binding("ItemName")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Дата поставки",
+                    DisplayMemberBinding = new Binding("DeliveryDate")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Тип предмета",
+                    DisplayMemberBinding = new Binding("WarehouseType.Type")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Количество",
+                    DisplayMemberBinding = new Binding("Quantity")
+                });
+
+                dataListView.ItemsSource = warehouse;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки данных: {ex.Message}", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
