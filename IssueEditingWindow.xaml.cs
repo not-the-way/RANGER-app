00fe9ead0900a8df6c9aa3dbe5f0da497b0cbb2d
not-the-way <txajbraxmanov@mail.ru>
2025-11-12 23:50:22 +0300

    Literally just created it

---
+++ IssueEditingWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RANGER
+{
+    /// <summary>
+    /// Логика взаимодействия для IssueEditingWindow.xaml
+    /// </summary>
+    public partial class IssueEditingWindow : Window
+    {
+        public IssueEditingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
